4b7845ff62c0063ba957f7b91aba479667bde2a4
Bandaru Abhilash <149057455+abhilash1432005@users.noreply.github.com>
2026-08-24 17:26:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 3
-  ami                    = "ami-0b6d9d3d33ba97d99"
-  instance_type          = "c7i-flex.large"
-  key_name               = "junks"
-  vpc_security_group_ids = ["sg-0a1fb8aa0587d50af"]
+  ami                    = "ami-01a00762f46d584a1 "
+  instance_type          = "t2.medium"
+  key_name               = "jenkins"
+  vpc_security_group_ids = ["sg-0af3a9c8da96e6df8"]
   tags = {
     Name = var.instance_names[count.index]
   }
@@ -17,9 +17,6 @@ variable "instance_names" {
   default = ["jenkins", "tomcat-1", "Monitoring server"]
 }
 
-resource "aws_s3_bucket" "one" {
-  bucket = "my-project-bucket-123456789"
-  versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket" "two" {
+  bucket = "Demo-jenkins-project"
 }
